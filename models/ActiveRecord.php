34fedb58ce5e884f2a968156de1fa98cdeea0201
LiaS1818 <sorgoroto@gmail.com>
2024-05-26 12:51:56 -0600

    debuguando

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -56,7 +56,7 @@ class ActiveRecord{
         $query .= " ) VALUES ('";  
         $query .= join("', '", array_values($atributos));   
         $query .= "')";
-        debuguear($query);
+        
 
         // $trigger = "
         // DELIMITER //
